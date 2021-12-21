b2429239a7de3aaa22ddb69161cd95e1002e4ab7
Alestry Pérez <alestry.perez@hyperisland.se>
2021-12-22 00:27:21 +0100

    init files

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DCS Kneeboard',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.grey,
+      ),
+      home: const MyHomePage(title: 'Kneeboard'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(),
+    );
+  }
+}
